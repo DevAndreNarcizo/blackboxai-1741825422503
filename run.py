45417167320c7243a6d5d173511093c29189b14f
André Narcizo <dev.andrenarcizo@gmail.com>
2025-03-12 21:24:03 -0300

    Add run.py 1741825442955

---
+++ run.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Script para executar o Fin Assist.
+Execute este arquivo para iniciar a aplicação.
+"""
+
+import sys
+from src.main import main
+
+if __name__ == "__main__":
+    main()
